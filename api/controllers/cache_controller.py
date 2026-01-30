16e14ece6d13f548e5ca0e124ceb86c83e1429c4
Cesar Guzman <cesar-gerardo@guzman-lopez.com>
2026-01-29 20:06:44 -0600

    BDE update

--- api/controllers/cache_controller.py
+++ api/controllers/cache_controller.py
@@ -5,8 +5,7 @@ from typing import Any
 
 from rdkit import logger
 
-
-CACHE_VERSION = "v6"
+CACHE_VERSION = "v7"
 CACHE_DIR = os.path.join(os.path.dirname(__file__), "_cache", CACHE_VERSION)
 CACHE_DB_PATH = os.path.join(CACHE_DIR, "cache.db")
 os.makedirs(CACHE_DIR, exist_ok=True)
